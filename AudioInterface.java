3ad0a0ba1039114d142c59a2122e468699908def
Alex Velazquez <alx.velazquez@gmail.com>
2014-04-02 13:31:14 -0400

    Delete AudioInterface.java

--- AudioInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AudioInterface extends Remote {
-  public Object StartRecord() throws RemoteException; /* will return a json string, use prefered json lib to parse it */
-  public Object getMetadata() throws RemoteException; /* will return a json string, return metadata for the last seconds of recording */
-  public byte[] StopRecord() throws RemoteException; /* Provide the file in bytes */
-}
